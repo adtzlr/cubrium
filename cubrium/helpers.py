1c9f8d6cb6e41f5bbd5720acf97d130ea075c01c
Andreas D <a.dutzler@gmail.com>
2021-03-05 16:23:40 +0100

    extend scripts

--- cubrium/helpers.py
+++ cubrium/helpers.py
@@ -9,16 +9,20 @@ import numpy as np
 
 
 def ddot(A, B):
+    "Returns the double-dot product of two tensors."
     return np.tensordot(A, B, 2)
 
 
 def dya(A, B):
+    "Returns the dyadic product of two tensors."
     return np.tensordot(A, B, 0)
 
 
 def cdya(A, B):
+    "Returns the cross-dyadic product of two tensors."
     return (np.einsum("ij,kl->ikjl", A, B) + np.einsum("ij,kl->ilkj", A, B)) / 2
 
 
 def dev(A):
+    "Returns the deviator of a tensor."
     return A - np.trace(A) / 3 * np.eye(3)
--- scripts/script102_shear.py
+++ scripts/script102_shear.py
@@ -29,27 +29,28 @@ def umat_svk(F, parameters):
     return F @ S
 
 
-MDL.GLO.constitution.umat = umat_svk
-MDL.GLO.constitution.parameters = [1.0, 5000.0]
+#MDL.GLO.constitution.umat = umat_svk
+MDL.GLO.constitution.matid = 1
+MDL.GLO.constitution.parameters = [1.0, 5000.0, 2]
 
 
-def uniaxial(MDL):
-    MDL.EXT.force.normal[0] = 1
-    MDL.EXT.force.normal[1] = 0
-    MDL.EXT.force.normal[2] = 0
+def shear(MDL):
 
-    MDL.EXT.force.shear[0, 1] = 0
-    MDL.EXT.force.shear[1, 2] = 0
-    MDL.EXT.force.shear[0, 2] = 0
+    MDL.EXT.force.normal[1] = 0.0
+    MDL.EXT.force.normal[2] = 0.0
+    MDL.EXT.force.shear[0, 1] = 1
 
-    MDL.EXT.gridvec.symmetry = [1, 1, 1]
+    MDL.EXT.gridvec.components[:, 0] = np.array([1, 0, 0])
+    MDL.EXT.gridvec.components[:2, 2] = np.array([0, 0])
 
-    MDL.GLO.lpftype = 0
-    MDL.GLO.title = "Uniaxial"
+    MDL.EXT.gridvec.components[2, 1] = 0
+
+    MDL.GLO.lpftype = 1
+    MDL.GLO.title = "Shear (no normal force on surfaces 2 and 3)"
     return MDL
 
 
-MDL = uniaxial(MDL)
+MDL = shear(MDL)
 MDL = cubrium.update(MDL)
 
 x0 = np.zeros(9)
@@ -58,9 +59,13 @@ lpf0 = 0.0
 Res = cubrium.solve(MDL)(
     x0=x0,
     lpf0=lpf0,
+    dlpfmax=0.02,
+    dxmax=0.02,
     rebalance=True,
-    maxsteps=200,
-    high=10,
+    maxsteps=50,
+    maxiter=8,
+    increase=1,
+    high=20,
     
 )
 
@@ -69,17 +74,10 @@ history = cubrium.recover(Y, MDL)
 
 import matplotlib.pyplot as plt
 
-plt.figure()
-plt.plot(1 + Y[:, 0], Y[:, -1], ".-")
-plt.xlabel("axial stretch $\lambda_1$")
+plt.plot(Y[:, 1], Y[:, -1], ".-")
+plt.xlabel("amount of shear $F_{12}$")
 plt.ylabel("load-proportionality-factor LPF")
-plt.savefig(MDL.GLO.title + "_stretch-normal-lpf.svg")
-
-plt.figure()
-plt.plot(1 + Y[:, 4], Y[:, -1], ".-")
-plt.xlabel("transversal stretch $\lambda_2 = \lambda_3$")
-plt.ylabel("load-proportionality-factor LPF")
-plt.savefig(MDL.GLO.title + "_stretch-transv-lpf.svg")
+plt.savefig(MDL.GLO.title + "_shear-lpf.svg")
 
 cubrium.writer.xdmf(
     history,
